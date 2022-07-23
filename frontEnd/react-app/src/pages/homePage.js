55a2074c6d1477ba986db72771c65f6b20dfca56
Ryan Monahan <59668423+RyanMonahan@users.noreply.github.com>
2022-07-23 16:33:15 -0500

    started work on home page UI

--- frontEnd/react-app/src/pages/homePage.js
+++ frontEnd/react-app/src/pages/homePage.js
@@ -1,7 +1,38 @@
 import React from "react";
+import Button from '@mui/material/Button';
+import logo from "../assets/SquareQuicknetLogo.png";
+
 
 function HomePage() {
-    return <div className="HomePage">Home Page</div>;
-  }
+    return (
+        <div className="HomePage" style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100vh',
+        }}>
+            <cl>
+                <h1>
+                    <img src={logo} style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        height: '20vh',
+                    }} />
+                </h1>
+                <h2>
+                    <Button variant="outlined" style = {{
+                        minWidth: '20vh',
+                    }}>Create Event</Button>
+                </h2>
+                <h3>
+                    <Button variant="outlined" style = {{
+                        minWidth: '20vh',
+                    }}>Login</Button>
+                </h3>
+            </cl>
+        </div>
+    );
+}
 
 export default HomePage;

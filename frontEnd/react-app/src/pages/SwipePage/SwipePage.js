4d7924f3f424b2790a3519cfa9c0e5154c256e4d
Michael French <michael.x.french@gmail.com>
2022-07-23 18:15:08 -0500

    buttons

--- frontEnd/react-app/src/pages/SwipePage/SwipePage.js
+++ frontEnd/react-app/src/pages/SwipePage/SwipePage.js
@@ -1,13 +1,45 @@
 import React from "react";
 import CompanyCard from "../../Components/CompanyCard";
-import './SwipePage.css';
+import "./SwipePage.css";
+import { ButtonGroup } from "@mui/material";
+import Button from "@mui/material/Button";
 
-function SwipePage(){
-    return (
-        <div className="swipe-page">
-            <CompanyCard/>
+function SwipePage() {
+  function handleRejectClick(evt) {
+    console.log("Reject Clicked");
+  }
+
+  function handleAcceptClick(evt) {
+    console.log("Accept Clicked");
+  }
+
+  return (
+    <div className="swipe-page">
+      <div className="swipe-page-contents">
+        <CompanyCard />
+        <div className="swipe-page-buttons">
+          <ButtonGroup className="d-flex">
+            <Button
+              className="p-2 flex-fill"
+              variant="contained"
+              color="error"
+              onClick={handleRejectClick}
+            >
+              Reject
+            </Button>
+            <Button
+              className="p-2 flex-fill"
+              variant="outlined"
+              color="success"
+              onClick={handleAcceptClick}
+            >
+              Accept
+            </Button>
+          </ButtonGroup>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default SwipePage;

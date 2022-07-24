f7002ba4e02379f3208a1530617e0a3a5b52b7c8
CodyScha <codscha@siue.edu>
2022-07-24 01:26:17 -0500

    removed some commented code oops

--- frontEnd/react-app/src/pages/homePage.js
+++ frontEnd/react-app/src/pages/homePage.js
@@ -28,36 +28,6 @@ function HomePage() {
                 </div>
             </div>
         </div>
-        // <div className="HomePage" style={{
-        //     display: 'flex',
-        //     alignItems: 'center',
-        //     justifyContent: 'center',
-        //     height: '100vh',
-        // }}>
-        //     <cl>
-        //         <h1>
-        //             <img src={logo} className="rounded" style={{
-        //                 display: 'flex',
-        //                 alignItems: 'center',
-        //                 justifyContent: 'center',
-        //                 height: '20vw',
-        //             }} />
-        //         </h1>
-        //         <h2>
-        //             <Link to={'/createevent'} className='text-decoration-none'>
-        //                 <Button variant="outlined" style={{
-        //                     minWidth: '20vw',
-        //                 }}>Create Event</Button>
-        //             </Link>
-
-        //         </h2>
-        //         <h3>
-        //             <Button variant="outlined" style={{
-        //                 minWidth: '20vh',
-        //             }}>Login</Button>
-        //         </h3>
-        //     </cl>
-        // </div>
     );
 }
 

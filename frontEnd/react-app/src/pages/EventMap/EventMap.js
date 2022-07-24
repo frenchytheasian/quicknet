af1994d65de61050668465b901c876ccb731d4cc
Michael French <michael.x.french@gmail.com>
2022-07-24 09:43:10 -0500

    finito

--- frontEnd/react-app/src/pages/EventMap/EventMap.js
+++ frontEnd/react-app/src/pages/EventMap/EventMap.js
@@ -1,9 +1,67 @@
 import React from "react";
+import { TableContainer, Table } from "@mui/material";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
 
 function EventMap() {
-    return (<div>
-        <h1>Event Map</h1>
-    </div>)
+  const companies = [
+    "Trevipay",
+    "H&R Block",
+    "Pinata",
+    "RX Savings Solutions",
+    "Ascend Learning",
+    "Symplr",
+    "GEHA",
+    "Spring Venture Group",
+    "Equipment Share",
+    "SS&C",
+    "CivicPlus",
+    "MongoDB",
+    "Veterans United",
+    "Quest Analytics",
+    "Federal Reserve Bank of Kansas City",
+    "Gitlab",
+    "Jack Henry",
+    "Tripleblind",
+    "Advantage Tech",
+    "Storewise",
+    "NAIC",
+    "Veeva",
+    "FNBO",
+    "CBOE",
+    "Torch.AI",
+    "Telnyx",
+    "Epiq",
+    "Ad Astra",
+    "Security Benefit",
+    "SunLife",
+    "Homebase",
+  ];
+  return (
+    <div>
+      <h1>Event Map</h1>
+      <TableContainer>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Company Name</TableCell>
+              <TableCell>Table Number</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {companies.map((company, ind) => (
+              <TableRow key={company}>
+                <TableCell>{company}</TableCell>
+                <TableCell>{ind + 1}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 }
 
 export default EventMap;
